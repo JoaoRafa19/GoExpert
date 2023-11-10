0706525ec5e49b3d3e667268294c85037b4a5e4a
JoaoRafa19 <joaopedrorafael19@gmail.com>
2023-11-10 11:56:52 -0300

    aula 3

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JoaoRafa19/goexpert01
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+const a = "Hello world"
+
+var b bool
+
+func main() {
+
+	println(b)
+	println(a)
+	b = true
+	println(b)
+
+}
